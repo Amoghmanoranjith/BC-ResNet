7718e4ff0223da0a40bdcad9b1ea1350053101d7
Amogh <130673768+Amoghmanoranjith@users.noreply.github.com>
2025-01-09 12:16:12 +0530

    Update util.py
    debugging the gpu problem

--- util.py
+++ util.py
@@ -2,4 +2,7 @@ import torch
 
 
 def get_device():
+    print(torch.cuda.is_available())  # Should return True if GPU is available
+    print(torch.cuda.current_device())  # Prints the current CUDA device (if any)
+    print(torch.cuda.get_device_name(0))  # Get the name of the GPU
     return "cuda" if torch.cuda.is_available() else "cpu"
